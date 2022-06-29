486a8040992b832df13086255bdbd3456eb02a1f
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2022-06-29 19:26:26 -0300

    Extract repos with starred data to a custom hook

--- components/RepoList.tsx
+++ components/RepoList.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from 'react'
 
-import { Repo } from '../entities/repo'
-import { getStarred, updateStarred } from '../repositories/stars'
+import { Repo, RepoId } from '../entities/repo'
+import * as stars from '../repositories/stars'
 
 import { RepoInfo } from './RepoInfo'
 
@@ -9,16 +9,21 @@ export type RepoListProps = {
   items: Array<Repo>
 }
 
-export const RepoList = ({ items }: RepoListProps) => {
-  const [starredRepoIds, setStarredRepoIds] = useState(new Set())
+const useReposWithStarredData = (items: Array<Repo>): [Array<Repo>, (repoId: RepoId, starred: boolean) => Promise<void>] => {
+  const [starredRepoIds, setStarredRepoIds] = useState<Set<RepoId>>(new Set())
   useEffect(() => {
-    getStarred().then(setStarredRepoIds)
+    stars.getStarred().then(setStarredRepoIds)
   }, [])
-  const repos = useMemo(() => {
+  const repos = useMemo<Array<Repo>>(() => {
     return items.map(repo => ({ ...repo, starred: starredRepoIds.has(repo.id) }))
   }, [items, starredRepoIds])
+  const updateStarred = (repoId: RepoId, starred: boolean) => stars.updateStarred(repoId, starred).then(setStarredRepoIds)
 
+  return [repos, updateStarred]
+}
 
+export const RepoList = ({ items }: RepoListProps) => {
+  const [repos, updateStarred] = useReposWithStarredData(items)
 
   return (
     <ul>
@@ -26,7 +31,7 @@ export const RepoList = ({ items }: RepoListProps) => {
         <li key={repo.id}>
           <RepoInfo
             repo={repo}
-            onStarredChange={(starred) => updateStarred(repo.id, starred).then(setStarredRepoIds)} />
+            onStarredChange={(starred) => updateStarred(repo.id, starred)} />
         </li>
       ))}
     </ul>
